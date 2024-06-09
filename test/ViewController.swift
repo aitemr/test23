37719cdf6a0102d85c9851d43b70a2a3d752b608
Islam <aitemr16@gmail.com>
2024-06-09 10:19:24 +0500

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Islam Temirbek on 09.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
